d1281b69d731a698aa14d815165f88c242cdc987
Harikakollipara <harikakollipara18@gmail.com>
2024-09-30 12:20:56 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter any two values \n");
+    printf("a=",a);
+    scanf("%d",&a);
+    printf("b=",b);
+    scanf("%d",&b);
+     c=a+b;
+    printf("the addition of two numbers are:%d",c);
+}
