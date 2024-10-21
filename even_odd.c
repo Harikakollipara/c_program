59b47a49cd737f598f18a81e520c4eda3df77ff3
Harikakollipara <harikakollipara18@gmail.com>
2024-10-21 11:47:18 +0530

    Create even_odd.c
    in this program 
    if statement check if the number is odd or not

---
+++ even_odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter a number",a);
+    scanf("%d",&a);
+    if(a%2!=0)
+    {
+        printf("the given number is odd");
+    }
+    else
+    {
+        printf("the given number is even");
+    }
+}
